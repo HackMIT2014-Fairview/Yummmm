2f2024a7482eb3b363d7d689c660ac3e3009466f
Naren Dasan <naren@narendasan.com>
2014-10-05 08:28:52 -0400

    scoring

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -15,11 +15,18 @@ class AnswersController < ApplicationController
     @lastQuestion = Question.order("updated_at").last
     start =  @lastQuestion.updated_at
     finish =  @answer.created_at
-    points = 100 - (finish - start)
+    if (finish - start) < 60
+        points = 100
+    elsif (finish - start) < 1800
+        points =  75
+    else
+        points = 50
+    end
+
     # for @users.each do |user|
       # user.max_score += 100
     # end
-    @completer = User.which(:subject => @lastQuestion.subject, :phone_number => from_number)
+    @completer = User.which(:subject => @lastQuestion.subject, :phone_number => from_number).first
     if @answer.response == @lastQuestion.answer
        @completer.score += points
        account_sid = 'AC6c15a831dc43075a3e628d59e410e8ef'
@@ -31,10 +38,10 @@ class AnswersController < ApplicationController
        @client.account.messages.create(
             :from => sender,
             :to => from_number,
-            :body => "Congrats, way to know your stuff! You have " + @completer.score + " points"
+            :body => "Congrats, way to know your stuff! You now have " + @completer.score + " points."
             )
     else
-        @completer.score -= points
+        @completer.score -= points / 4
         account_sid = 'AC6c15a831dc43075a3e628d59e410e8ef'
         auth_token = '8cc5c6bfc1b4659c35946fc12c5986ae'
 
@@ -44,7 +51,7 @@ class AnswersController < ApplicationController
         @client.account.messages.create(
               :from => sender,
               :to => from_number,
-              :body => "Too Bad, maybe you should study more. You have " + @completer.score + "points"
+              :body => "Better luck next time! You now have " + @completer.score + " points."
               )
 
     end
